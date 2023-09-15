56cdc29b4f11ade76653316d8a01aa8ea352fc22
ik3nna <iamiykendukwu@gmail.com>
2023-09-15 13:30:45 +0100

    feat: Initial setup of marketplace page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Footer from "./components/footer";
 
 // pages
 import Home from "./pages/home";
+import Marketplace from "./pages/marketplace";
 
 const App: React.FC =()=> {
 
@@ -16,6 +17,7 @@ const App: React.FC =()=> {
       
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/marketplace" element={<Marketplace />} />
 
         <Route path="/" element= {<Navigate replace to="/home" />} />
       </Routes>
---
+++ src/pages/marketplace/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./index.module.css"
+
+const Marketplace: React.FC = () => {
+  return (
+    <section className={styles.container}>
+
+    </section>
+  )
+}
+
+export default Marketplace;
