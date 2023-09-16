ab3c2c28a5ecc94eca6f85061b9aae7a000d2574
ik3nna <iamiykendukwu@gmail.com>
2023-09-16 12:57:02 +0100

    feat: UI setup

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import styles from "./index.module.css"
+import styles from "./index.module.css";
+
+// assets
+import filterIcon from "../../assets/filter.svg";
+
+// icons
+import { BiSearch } from "react-icons/bi"
 
 const Marketplace: React.FC = () => {
   return (
     <section className={styles.container}>
+        <article>
+            <div className={styles.search}>
+                <input type='text' placeholder="Search" />
+                <BiSearch className={styles.search_icon} />
+            </div>
 
+            <div className={styles.filter_container}>
+                <img src={filterIcon} alt="filter" />
+                <div>Filter</div>
+            </div>
+
+        </article>
+
+        <article>
+            <div className={styles.viewbox}>
+                See 1-16 of 15 results
+            </div>
+        </article>
     </section>
   )
 }
