6a921b4ddc4ca1e306480640ea927947d3ce0d7d
ik3nna <iamiykendukwu@gmail.com>
2023-09-18 13:09:42 +0100

    feat: mock data

---
+++ src/utils/data.ts
@@ -0,0 +1,304 @@
+import philomenaImg from "../assets/items/philomena.svg";
+import booleanImg from "../assets/items/boolean.svg";
+import blancImg from "../assets/items/blanc.svg";
+import ellipsiaImg from "../assets/items/ellipsa.svg";
+import lawmakersImg from "../assets/items/lawmakers.svg";
+import veilImg from "../assets/items/veil.svg";
+import alternatingImg from "../assets/items/alternating.svg";
+import rosemaryImg from "../assets/items/rosemary.svg";
+import beverlyImg from "../assets/Rectangle 230.svg";
+import image2 from "../assets/Rectangle 231.svg";
+import image3 from "../assets/Rectangle 232.svg";
+import image4 from "../assets/Rectangle 233.svg";
+import image5 from "../assets/Rectangle 234.svg";
+
+export const Data = [
+    {
+        id: 1,
+        category: "Editorials",
+        artist: "Philomena '22",
+        price: "$90.00",
+        image: philomenaImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 2,
+        category: "Fashion",
+        artist: "Boolean Egyptian",
+        price: "$130.00",
+        image: booleanImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 3,
+        category: "Optics",
+        artist: "Blanc",
+        price: "$150.00",
+        image: blancImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 4,
+        category: "Art & Museum",
+        artist: "Ellipsia",
+        price: "$170.00",
+        image: ellipsiaImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 5,
+        category: "Nature",
+        artist: "The Lawmakers",
+        price: "$200.00",
+        image: lawmakersImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 6,
+        category: "Editorials",
+        artist: "Veil",
+        price: "$110.00",
+        image: veilImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 7,
+        category: "Fashion",
+        artist: "Alternating",
+        price: "$120.00",
+        image: alternatingImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 8,
+        category: "Optics",
+        artist: "Rosemary '22",
+        price: "$250.00",
+        image: rosemaryImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 9,
+        category: "Art & Museum",
+        artist: "Beverly",
+        price: "$210.00",
+        image: beverlyImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 10,
+        category: "Nature",
+        artist: "The Lawmakers",
+        price: "$80.00",
+        image: image2,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 11,
+        category: "Nature",
+        artist: "The Lawmakers",
+        price: "$220.00",
+        image: image3,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 12,
+        category: "Editorials",
+        artist: "Veil",
+        price: "$300.00",
+        image: image4,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 13,
+        category: "Fashion",
+        artist: "Alternating",
+        price: "$80.00",
+        image: image5,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 14,
+        category: "Optics",
+        artist: "Rosemary '22",
+        price: "$50.00",
+        image: rosemaryImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 15,
+        category: "Art & Museum",
+        artist: "Beverly",
+        price: "$100.00",
+        image: beverlyImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 16,
+        category: "Nature",
+        artist: "The Lawmakers",
+        price: "$180.00",
+        image: image2,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 17,
+        category: "Optics",
+        artist: "Blanc",
+        price: "$250.00",
+        image: blancImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+    {
+        id: 18,
+        category: "Art & Museum",
+        artist: "Ellipsia",
+        price: "$270.00",
+        image: ellipsiaImg,
+        info: {
+            desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            listings: `Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur`,
+            status: "Available"
+        }
+    },
+]
